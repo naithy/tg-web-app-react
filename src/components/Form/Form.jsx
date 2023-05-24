7752a1f7f98edc56a094d1247c93ca4ab9b6c367
naithy <naithyDev@gmail.com>
2023-05-24 19:55:03 +0400

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -24,7 +24,7 @@ const Form = () => {
 
     useEffect(() => {
         tg.MainButton.setParams({
-            text: 'Отправтиь данные'
+            text: 'Отправить данные'
         })
     }, [])
 
