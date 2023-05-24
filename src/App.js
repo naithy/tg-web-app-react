02b671c7bb2c14ad6dbf7f11e47400fc873146e2
naithy <naithyDev@gmail.com>
2023-05-25 00:44:03 +0400

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
-import Header from "./components/Header/Header";
 import {Route, Routes} from "react-router-dom";
 import ProductList from "./components/ProductList/ProductList";
 import Form from "./components/Form/Form";
@@ -16,7 +15,6 @@ function App() {
 
   return (
     <div className="App">
-        <Header />
         <Routes>
             <Route index element={<ProductList/>}/>
             <Route path={'form'} element={<Form/>}/>
