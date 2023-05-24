01945ee4c6c223d266904e4508ac6bd2be36f059
naithy <naithyDev@gmail.com>
2023-05-24 19:29:34 +0400

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './Form.css';
+import {useTelegram} from "../../hooks/useTelegram";
 
 const Form = () => {
+    const [name, setName] = useState();
+    const [phoneNumber, setPhoneNumber] = useState();
+    const {tg} = useTelegram();
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Отправтиь данные'
+        })
+    }, [])
+
+    useEffect(() => {
+        if (!name || !phoneNumber) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    })
+    const onChangeName = (e) => {
+        setName(e.target.value)
+    }
+
+    const onChangePhoneNumber = (e) => {
+        setPhoneNumber(e.target.value)
+    }
+
     return (
-        <div>
-            Form
+        <div className={"form"}>
+            <h3>Введите ваши данные</h3>
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Имя'}
+                value={name}
+                onChange={onChangeName}
+            />
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Номер телефона'}
+                value={phoneNumber}
+                onChange={onChangePhoneNumber}
+            />
         </div>
     );
 };
