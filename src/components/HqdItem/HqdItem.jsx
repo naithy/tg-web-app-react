e90e859bad54f4050ca1786b8c7dd3552ca077aa
naithy <naithyDev@gmail.com>
2023-05-28 22:00:50 +0400

    init

--- src/components/HqdItem/HqdItem.jsx
+++ src/components/HqdItem/HqdItem.jsx
@@ -9,20 +9,23 @@ const HqdItem = ({product, className, onAdd}) => {
     }
 
     return (
-        <a onClick={() => history(product.id)} className={'toItemPage'}>
-            <div className={'hqdcontainer'}>
-                <div className={'product ' + className}>
-                    <img className={'hqdimg'} src={product.img}/>
-                    <div className={'hqdcontainertext'}>
-                        <div className={`hqdtitle`}>{product.title}</div>
-                        {/*<div className={'description'}>{product.description}</div>*/}
-                        <div className={'price'}>
-                            <span>{product.price + 'р.'}</span>
+        <div>
+            <a onClick={() => history(product.id)} className={'toItemPage'}>
+                <div className={'hqdcontainer'}>
+                    <div className={'product ' + className}>
+                        <img className={'hqdimg'} src={product.img}/>
+                        <div className={'hqdcontainertext'}>
+                            <div className={`hqdtitle`}>{product.title}</div>
+                            {/*<div className={'description'}>{product.description}</div>*/}
+                            <div className={'price'}>
+                                <span>{product.price + 'р.'}</span>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </a>
+            </a>
+            <button onClick={() => history(-1)}>Go back</button>
+        </div>
     );
 };
 
