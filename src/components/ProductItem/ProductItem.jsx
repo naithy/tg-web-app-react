ea00a314915d3b8685e05f8d84544f3409e40087
naithy <naithyDev@gmail.com>
2023-06-03 22:16:55 +0400

    init

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -6,7 +6,12 @@ import {useTelegram} from "../../hooks/useTelegram";
 
 
 const ProductItem = () => {
+
+    const element = document.getElementsByClassName('choicecontainer')
+    let color = window.getComputedStyle(element).background
     const {tg} = useTelegram();
+    tg.showAlert(color)
+
 
     const productsData = [
         { title: 'gang box x 800', price: 900, flavors: ['vanilla', 'cherry', 'apple'] },
