a721f2b25bc0aebef851aea317de8ca8ba92d465
naithy <naithyDev@gmail.com>
2023-06-04 12:31:57 +0400

    init

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import './Button.css';
 
 const Button = (props) => {
     return (
-        <div>
+        <div className={'btn'}>
             <button {...props} className={'button ' + props.className}/>
         </div>
     );
