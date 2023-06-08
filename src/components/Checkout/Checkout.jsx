7d7f75ab3b3b7b3827bf25dbe927d44562248226
naithy <naithyDev@gmail.com>
2023-06-08 14:37:37 +0400

    init

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -3,7 +3,7 @@ import './Checkout.css'
 
 const Checkout = () => {
     return (
-        <div>
+        <div className={'checkout'}>
             <div className={'ordertitle'}>
                 <div className={'ordertext'}>Ваш заказ</div>
                 <button className={'editorder'}>Изменить</button>
