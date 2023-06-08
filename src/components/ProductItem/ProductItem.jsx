309d142539b44c25b7c1e0c2da90690a7f65e779
naithy <naithyDev@gmail.com>
2023-06-08 15:02:11 +0400

    init

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {useNavigate, useParams} from 'react-router-dom';
 import './ProductItem.css'
 import Button from "../Button/Button";
