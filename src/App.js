a1622022bd42fe217b7f8f6c03dd4e0c34804d67
naithy <naithyDev@gmail.com>
2023-06-09 17:36:50 +0400

    init

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import './App.css';
 import React, {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
-import {Route, Routes, useNavigate} from "react-router-dom";
+import {Route, Routes, useNavigate, useLocation} from "react-router-dom";
 import CategoryList from "./components/CategoryList/CategoryList";
 import ProductPage from "./components/ProductPage/ProductPage";
 import ProductItem from "./components/ProductItem/ProductItem";
 import Checkout from "./components/Checkout/Checkout";
 
 function App() {
-    const history = useNavigate()
+    const history = useNavigate();
+    const location = useLocation();
 
     const {tg} = useTelegram();
     useEffect(() => {
         tg.ready();
     })
-
-    tg.MainButton.onClick(() => history('/checkout'));
+    if (location.pathname !== '/checkout') {
+        tg.MainButton.onClick(() => history('/checkout'));
+    }
     tg.enableClosingConfirmation();
 
 
