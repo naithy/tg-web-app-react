924ca5bceb96d3b6f27db912a492878abd899c47
naithy <naithyDev@gmail.com>
2023-06-09 17:58:03 +0400

    init

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -127,16 +127,7 @@ const Checkout = () => {
         })
     }, [Cart])
 
-     // if (location.pathname === '/checkout') {
-     //     useEffect(() => {
-     //         tg.onEvent('mainButtonClicked', onSendData)
-     //         tg.onEvent('mainButtonClicked', tg.showAlert('Заказ был оформлен, ожидайте связи с менеджером'))
-     //         return () => {
-     //             tg.offEvent('mainButtonClicked', onSendData)
-     //             tg.offEvent('mainButtonClicked', tg.showAlert('Заказ был оформлен, ожидайте связи с менеджером'))
-     //         }
-     //     }, [onSendData])
-     // }
+
 
     return (
         <div className={'checkout'}>
