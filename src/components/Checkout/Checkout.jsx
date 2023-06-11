b53b93ef131c1a5e21951e0b57987b4a08db0e05
naithy <naithyDev@gmail.com>
2023-06-11 15:59:27 +0400

    init

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -75,6 +75,9 @@ const Checkout = () => {
         if (savedNumberValue) {
             setSavedNumber(savedNumberValue);
         }
+        return () => {
+
+        }
     }, []);
 
     useEffect(() => {
