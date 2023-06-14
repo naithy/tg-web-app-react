b4ae732353d9afaa5bf4643fe0182c523d54d6c8
naithy <naithyDev@gmail.com>
2023-06-14 16:02:42 +0400

    init

--- src/components/ProductPage/ProductPage.jsx
+++ src/components/ProductPage/ProductPage.jsx
@@ -1,17 +1,30 @@
 import {useTelegram} from "../../hooks/useTelegram";
 import {Link, useNavigate, useParams} from "react-router-dom";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './ProductPage.css'
 import {motion} from "framer-motion";
 
-const productsData = [
-    { title: 'gang box x 800', price: 900, flavors: ['vanilla', 'cherry', 'apple'],
-        img: 'https://sun9-66.userapi.com/impg/daUL-0rsVWF4iFxoIBNOpsYf93LJ_8yOyCnTkA/Qr3P8UJK2_I.jpg?size=1280x1280&quality=96&sign=10b465a1219b01ac2e85cd7a3ee6ebc1&type=album'
-    },
-    { title: 'lost mary b5000', price: 1000, flavors: ['vanilla', 'cherry', 'blueberry'] },
-    { title: 'voopoo', price: 950, flavors: ['mango'] },
-    { title: 'vaporesso', price: 900 },
-];
+// const productsData = [
+//     { title: 'gang box x 800', price: 900, flavors: ['vanilla', 'cherry', 'apple'],
+//         img: 'https://sun9-66.userapi.com/impg/daUL-0rsVWF4iFxoIBNOpsYf93LJ_8yOyCnTkA/Qr3P8UJK2_I.jpg?size=1280x1280&quality=96&sign=10b465a1219b01ac2e85cd7a3ee6ebc1&type=album'
+//     },
+//     { title: 'lost mary b5000', price: 1000, flavors: ['vanilla', 'cherry', 'blueberry'] },
+//     { title: 'voopoo', price: 950, flavors: ['mango'] },
+//     { title: 'vaporesso', price: 900 },
+// ];
+
+const [productsData, setProductsData] = useState([]);
+
+useEffect(() => {
+    fetch('/products?category=disposable')
+        .then((response) => response.json())
+        .then((data) => {
+            setProductsData(data);
+        })
+        .catch((error) => {
+            console.error('Error fetching products:', error);
+        });
+}, []);
 
 const ProductPage = () => {
     const {tg} = useTelegram();
