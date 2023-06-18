7bafce48dfb595f61934f9a7a2b46ae930983c01
naithy <naithyDev@gmail.com>
2023-06-18 20:28:42 +0400

    init

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -88,23 +88,27 @@ const ProductItem = () => {
         handleIncrement(flavor)
         tg.HapticFeedback.impactOccurred('light')
         const newCart = { ...cart };
-        if (newCart[product._id] && newCart[product._id].flavors[flavor] && newCart[product._id].flavors[flavor] < quantity) {
-            newCart[product._id].flavors[flavor]++;
+        if (newCart[product._id].flavors[flavor] === quantity) {
+
         } else {
-            if (!newCart[product._id]) {
-                newCart[product._id] = {};
-                newCart[product._id].title = product.title;
-                newCart[product._id].price = product.price;
-                newCart[product._id].flavors = {};
+            if (newCart[product._id] && newCart[product._id].flavors[flavor]) {
+                newCart[product._id].flavors[flavor]++;
+            } else {
+                if (!newCart[product._id]) {
+                    newCart[product._id] = {};
+                    newCart[product._id].title = product.title;
+                    newCart[product._id].price = product.price;
+                    newCart[product._id].flavors = {};
+                }
+                newCart[product._id].flavors[flavor] = 1;
             }
-            newCart[product._id].flavors[flavor] = 1;
+            setCart(newCart);
+            sessionStorage.setItem('cart', JSON.stringify(newCart));
+            const total = calculateTotalPrice(newCart);
+            setTotalPrice(total);
+            sessionStorage.setItem('totalPrice', JSON.stringify(total));
+            dispatchEvent(new Event("storage"))
         }
-        setCart(newCart);
-        sessionStorage.setItem('cart', JSON.stringify(newCart));
-        const total = calculateTotalPrice(newCart);
-        setTotalPrice(total);
-        sessionStorage.setItem('totalPrice', JSON.stringify(total));
-        dispatchEvent(new Event("storage"))
     }
 
     const handleRemoveFromCart = (flavor) => {
