1b951e3d852a6f794af1c09e16583dac46c2d1af
naithy <naithyDev@gmail.com>
2023-06-20 18:21:49 +0400

    init

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,10 +1,10 @@
-import React, {useEffect} from 'react';
-import {Link} from "react-router-dom";
+import React, {useEffect, useState} from 'react';
+import {Link, useLocation} from "react-router-dom";
 
 const Products = () => {
     const [products, setProducts] = useState([]);
 
-    const productsf = useLocation().state.productsTo;
+    const productsf = useLocation.state.productsTo;
     const brand = useLocation().state.filterBrand;
 
     useEffect(() => {
